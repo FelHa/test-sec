40dfd76316aa920b6760db03ce1b74ed2c46ab15
Felix Haußer <f.hausser@dotsource.de>
2025-06-10 14:21:06 +0200

    log secret #2

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ import express from 'express'
 
 const app = express()
 
+// This is a fake AWS key for testing GitHub Secret Protection - DO NOT USE REAL SECRETS
+const fakeAwsKey = 'AKIAIOSFODNN7EXAMPLE'
+
+// This is a fake GitHub token for testing GitHub Secret Protection
+const fakeGitHubToken = 'ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789'
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
@@ -9,3 +15,6 @@ app.get('/', (req, res) => {
 app.listen(4000)
 
 console.log(process.env.SSH_PRIVATE_KEY)
+// Logging fake secrets for testing purposes only
+console.log('Fake AWS Key:', fakeAwsKey)
+console.log('Fake GitHub Token:', fakeGitHubToken)
